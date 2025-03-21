6d34850d8641b39575a2ea6cb47da4330abff41b
SafetImamovic <safet.imamovic.22@size.ba>
2025-03-21 20:55:32 +0100

    Change CMakeLists, add time measurements

--- src/main.c
+++ src/main.c
@@ -6,17 +6,76 @@ int main()
 
         CORE.TERMINAL.init_console();
 
+        // TODO: Pretify this whole thing.
+#ifdef _WIN32
+
+        LARGE_INTEGER frequency, start, end;
+
+        QueryPerformanceFrequency(&frequency);
+
+#else
+
+        struct timespec start, end;
+
+        // Set up the signal handler for SIGWINCH
+        struct sigaction sa;
+
+        sa.sa_handler = handle_winch;
+
+        sa.sa_flags   = 0;
+
+        sigemptyset(&sa.sa_mask);
+
+        if (sigaction(SIGWINCH, &sa, NULL) == -1)
+        {
+                perror("sigaction");
+
+                return 1;
+        }
+
+        // Print the initial size of the terminal
+        handle_winch();
+
+#endif
+
+        double elapsedTime = 0.0f;
+
         while (1)
         {
+
+#ifdef _WIN32
+
+                QueryPerformanceCounter(&start);
+
+#else
+
+                clock_gettime(CLOCK_MONOTONIC, &start);
+
+#endif
+
                 CORE.TERMINAL.clear_buffer();
 
                 CORE.UTILS.draw_coordinate_system();
 
-                CORE.UTILS.draw_stats(0.0f);
+                CORE.UTILS.draw_stats(elapsedTime);
 
                 CORE.UTILS.draw_edges('|', '-', '+');
 
                 CORE.TERMINAL.render_buffer();
+
+#ifdef _WIN32
+
+                QueryPerformanceCounter(&end);
+
+                elapsedTime = (double)(end.QuadPart - start.QuadPart) * 1000.0 / frequency.QuadPart;
+
+#else
+
+                clock_gettime(CLOCK_MONOTONIC, &end);
+
+                elapsedTime = (end.tv_sec - start.tv_sec) * 1000.0 + (end.tv_nsec - start.tv_nsec) / 1000000.0;
+
+#endif
         }
 
         CORE.TERMINAL.show_cursor();
