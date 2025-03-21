79da5e395cfeebef21a6b26da788016a16fe7b48
SafetImamovic <safet.imamovic.22@size.ba>
2025-03-21 20:58:48 +0100

    Made it work on Linux

--- include/core/terminal.h
+++ include/core/terminal.h
@@ -6,6 +6,12 @@
 #ifdef _WIN32
 #include <windows.h>
 #include <conio.h>
+#else
+#include <sys/ioctl.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <time.h>
 #endif
 
 void init_console();
--- src/main.c
+++ src/main.c
@@ -20,9 +20,7 @@ int main()
         // Set up the signal handler for SIGWINCH
         struct sigaction sa;
 
-        sa.sa_handler = handle_winch;
-
-        sa.sa_flags   = 0;
+        sa.sa_flags = 0;
 
         sigemptyset(&sa.sa_mask);
 
@@ -32,9 +30,6 @@ int main()
 
                 return 1;
         }
-
-        // Print the initial size of the terminal
-        handle_winch();
 
 #endif
 
